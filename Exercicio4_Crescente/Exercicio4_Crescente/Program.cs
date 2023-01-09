74df1048a29c9944374cb6fa6e61e947399d739d
TalisonMoura <111541453+TalisonMoura@users.noreply.github.com>
2023-01-09 17:26:54 -0300

    Add files via upload

---
+++ Exercicio4_Crescente/Exercicio4_Crescente/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Exercicio4_Crescente {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int x, y;
+
+            Console.WriteLine("Digite dois números:");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            while (x != y) {
+            
+                if(x>y) {
+                    Console.WriteLine("DECRESCENTE!");
+                }
+                else {
+                    Console.WriteLine("CRESCENTE!");
+                }
+                Console.WriteLine("DIGITE OUTROS DOIS NÚMEROS:");
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+            }
+
+
+        }
+    }
+}
---
+++ Exercicio5_SomaImpares/Exercicio5_SomaImpares/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicio5_SomaImpares {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int x, y, soma=0, change;
+
+            Console.WriteLine("Digite dois Números");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x > y) {
+                change = x;
+                x = y;
+                y = change;
+            }
+            for (int i = x + 1; i < y; i++) {
+                if(i %2 != 0) {
+                    soma = soma + i;
+                }
+            }
+
+            Console.WriteLine("SOMA DOS IMPARES = " + soma);
+        }
+    }
+}
