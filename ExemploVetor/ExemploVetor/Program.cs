7d039a29bd5154971bca0e81559645e6ff032c94
TalisonMoura <111541453+TalisonMoura@users.noreply.github.com>
2023-01-09 17:24:11 -0300

    Add files via upload

---
+++ ExemploVetor/ExemploVetor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace ExemploVetor {
+    internal class Program {
+        static void Main(string[] args) {
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            int i,n;
+            Console.Write("Quantos numeros voce vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[n];
+
+            for(i=0;i<n;i++) {
+                Console.Write("Digite um numero: ");
+                vet[i] = double.Parse(Console.ReadLine(), ci);
+            }
+            Console.WriteLine();
+            Console.WriteLine("NUMEROS DIGITADOS:");
+            for(i=0;i<n;i++) {
+                Console.WriteLine(vet[i].ToString("F1"), ci) ;
+            }
+        }
+    }
+}
---
+++ Exercicio1_Retangulo/Exercicio1_Retangulo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace Exercicio1_Retangulo {
+    internal class Program {
+        static void Main(string[] args) {
+            CultureInfo ci = CultureInfo.InvariantCulture;      
+
+            double largura, altura, area, perimetro, diagonal;
+
+            Console.Write("Base do Retangulo: ");
+            largura = double.Parse(Console.ReadLine(), ci);
+            Console.Write("Altura do Retangulo: ");
+            altura = double.Parse(Console.ReadLine(), ci);  
+
+            area = largura * altura;
+            perimetro = 2 * (largura + altura);
+            diagonal = Math.Sqrt(Math.Pow(largura, 2.0) + Math.Pow(altura, 2.0));
+
+            Console.WriteLine("Area = " + area.ToString("F4", ci));
+            Console.WriteLine("Perimetro = " + perimetro.ToString("F4", ci));
+            Console.WriteLine("Diagonal = " + diagonal.ToString("F4", ci));
+
+
+        }
+    }
+}
