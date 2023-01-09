e83b8935754ef06596bef88b74d6ab71cf1f3b71
TalisonMoura <111541453+TalisonMoura@users.noreply.github.com>
2023-01-09 17:28:29 -0300

    Add files via upload

---
+++ Exercicio6_SomaVetor/Exercicio6_SomaVetor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Exercicio6_SomaVetor {
+    internal class Program {
+        static void Main(string[] args) {
+            CultureInfo ci = CultureInfo.InvariantCulture;  
+
+            int i, n;
+            double soma=0, media=0;
+            Console.Write("Quantos números você vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+            
+            double[] vet = new double[n];
+
+            for(i=0;i<n; i++) {
+                Console.Write("Digite um numero: ");
+                vet[i] = double.Parse(Console.ReadLine(), ci);
+            }
+            Console.Write("\nVALORES = ");
+            for(i=0;i<n; i++) {
+                Console.Write(vet[i].ToString("f1", ci)+ " || ");
+            }
+            for(i=0;i<n; i++) {
+                soma  = soma + vet[i];
+            }
+            media = soma / n;
+            Console.WriteLine("\nSOMA = " + soma.ToString("f2", ci));
+            Console.WriteLine("MEDIA = " + media.ToString("f2", ci));
+
+
+        }
+    }
+}
---
+++ Exercicio7_Duracao/Exercicio7_Duracao/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio7_Duracao {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int segundos,minutos,horas,resto,num;
+            Console.Write("Digite a duaração em segundos: ");
+            num = int.Parse(Console.ReadLine());
+
+            horas = num / 3600;
+            resto = num % 3600;
+            minutos = resto / 60;
+            segundos = resto % 60;
+
+            Console.Write(horas+":"+minutos+":"+segundos);
+        
+        
+        
+        }
+    }
+}
